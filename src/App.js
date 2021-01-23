14c485a6059d277e4b1a816aa6c75e1ce3a96f46
Samantha <samantha.desouz@gmail.com>
2021-01-23 19:24:00 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import Contact from './components/Contact'
+import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-<Contact />
+
+<ContactList />
+
+{/* <Contact avatar='https://randomuser.me/api/portraits/men/67.jpg'
+name='Jeffrey Holt'
+online />
+<Contact avatar='https://randomuser.me/api/portraits/men/31.jpg'
+name='Kurt Russell'
+offline />
+<Contact avatar='https://randomuser.me/api/portraits/men/18.jpg'
+name='Matthew Castillo'
+online /> */}
 
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,26 @@ import './chat-app.css'
 
 const online = true; 
 
-function Contact (){
+function Contact (props){
     return (
 <div className='Contact'>
+<img className='avatar' src={props.avatar}/>
+    <div className='right'>
+    <p className='name'>{props.name}</p>
+        <div className='status'>
+            <p className='status-text'>
+            {props.online ? <span className='status-online'></span> : <span className='status-offline'></span>}
+            </p>
+            <p className='status-text'>{props.online ? "Online" : "Offline"}</p>
+        </div>
+    </div>
+</div>
+    );
+}
+
+export default Contact; 
+
+{/* <div className='Contact'>
 <img className='avatar' src='https://randomuser.me/api/portraits/men/57.jpg'/>
     <div className='right'>
         <p className='name'>Todd Sims</p>
@@ -15,8 +32,4 @@ function Contact (){
             <p className='status-text'>{online ? "Online" : "Offline"}</p>
         </div>
     </div>
-</div>
-    );
-}
-
-export default Contact; 
+</div> */}
