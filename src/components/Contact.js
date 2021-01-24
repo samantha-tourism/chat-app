60782e48744993f51eb0517ecad8beca26eb3501
Samantha <samantha.desouz@gmail.com>
2021-01-24 18:38:52 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,54 @@
 import './chat-app.css'
+import React from "react"
 
-const online = true; 
-
-function Contact (props){
-    return (
-<div className='Contact'>
-<img className='avatar' src={props.avatar}/>
-    <div className='right'>
-    <p className='name'>{props.name}</p>
-        <div className='status'>
-            <p className='status-text'>
-            {props.online ? <span className='status-online'></span> : <span className='status-offline'></span>}
-            </p>
-            <p className='status-text'>{props.online ? "Online" : "Offline"}</p>
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            online:false,
+        }
+    }
+    render(){
+        return (
+    <div className='Contact'>
+    <img className='avatar' src={this.props.avatar}/>
+        <div className='right'>
+        <p className='name'>{this.props.name}</p>
+            <div className='status' >
+            <p className='status-text' onClick={event => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+            }} >
+             <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+             <div className='status-text'> {this.state.online ? 'Online' : 'Offline'} </div>
+                </p>
+            </div>
         </div>
     </div>
-</div>
-    );
+        );
+    }
 }
+
+// function Contact (props){
+//     return (
+// <div className='Contact'>
+// <img className='avatar' src={props.avatar}/>
+//     <div className='right'>
+//     <p className='name'>{props.name}</p>
+//         <div className='status'>
+//             <p className='status-text'>
+//             {props.online ? <span className='status-online'></span> : <span className='status-offline'></span>}
+//             </p>
+//             <p className='status-text'>{props.online ? "Online" : "Offline"}</p>
+//         </div>
+//     </div>
+// </div>
+//     );
+// }
 
 export default Contact; 
 
-{/* <div className='Contact'>
+/* <div className='Contact'>
 <img className='avatar' src='https://randomuser.me/api/portraits/men/57.jpg'/>
     <div className='right'>
         <p className='name'>Todd Sims</p>
@@ -32,4 +59,4 @@ export default Contact;
             <p className='status-text'>{online ? "Online" : "Offline"}</p>
         </div>
     </div>
-</div> */}
+</div> */
